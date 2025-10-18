5f0cb62f286b2852b6e4dcd7d33c3e4c356256a1
Devil-666-sk <sk6930037@gmail.com>
2025-10-18 13:23:02 +0530

    site map fetch

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://infravibetech.com", // 🔹 apni live domain
-  generateRobotsTxt: true, // Robots.txt auto generate karega
-  generateIndexSitemap: false, // ❌ index sitemap (sitemap-0.xml) disable
+  siteUrl: "https://infravibetech.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true, // ✅ allow index sitemap
   sitemapSize: 5000,
-  outDir: "public", // sitemap yahi generate karega
+  outDir: "public",
   changefreq: "weekly",
   priority: 0.8,
+  autoLastmod: true,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
 
 export default config;
